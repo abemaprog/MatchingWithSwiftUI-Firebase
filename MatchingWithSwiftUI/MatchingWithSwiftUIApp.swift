74a62157af9d3a6349ad9dbcacee5bacd19f6d48
abemaprog <appdev.abe@gmail.com>
2024-10-25 11:17:00 +0900

    Initial Commit

---
+++ MatchingWithSwiftUI/MatchingWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingWithSwiftUIApp.swift
+//  MatchingWithSwiftUI
+//
+//  Created by Manato Abe on 2024/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MatchingWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
